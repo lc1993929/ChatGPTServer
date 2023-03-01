62511484e235213eea256f16eb2d8d688a4d44b4
刘畅 <736779328@qq.com>
2023-03-01 23:08:03 +0800

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the repository from GitHub
+git clone https://github.com/user/repo.git
+
+# Change directory to the cloned repository
+cd repo
+
+# Build the Go application
+go build -o app
+
+# Execute the compiled binary
+./app
